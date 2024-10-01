a42ef805ad2824724e71397167eed90f3d97c5b4
Eduard Murillo <murilloeduard3@gmail.com>
2024-10-01 11:25:58 -0500

    Update objetos.js

--- objetos.js
+++ objetos.js
@@ -1,28 +1,28 @@
-let persona ={
+let persona = {
     nombre: "Camilo",
-    apellido:"rocha",
-    telefono:32656514,
-    email:"cerorocha@distrital.edu.co",
-    edad:25
+    apellido: "rocha",
+    telefono: 32656514,
+    email: "cerorocha@distrital.edu.co",
+    edad: 25
 
 }
-let persona3 = new  Object ();
+let persona3 = new Object();
 persona3.nombre = "jorge";
-persona3.apellido ="rocha";
+persona3.apellido = "rocha";
 
 
-let persona2 ={
+let persona2 = {
     nombre: "Camilo",
-    apellido:"rocha",
-    telefono:32656514,
-    email:"cerorocha@distrital.edu.co",
-    edad:25,
-    nombreCompleto: function  (){
+    apellido: "rocha",
+    telefono: 32656514,
+    email: "cerorocha@distrital.edu.co",
+    edad: 25,
+    nombreCompleto: function () {
         return this.nombre + " " + this.apellido;
     }
 }
 console.log(persona2.nombreCompleto())
-for (nombrePropiedad in persona){
+for (nombrePropiedad in persona) {
     console.log(nombrePropiedad)
     console.log(persona[nombrePropiedad])
 }
@@ -36,21 +36,50 @@ console.log(personaArray)
 
 let personaString = JSON.stringify(persona)
 console.log(personaString)
-function Persona (nombre = 'Juan', apellido, email) {
+function Persona(nombre = 'Juan', apellido, email) {
     this.nombre = nombre;
     this.apellido = apellido
     this.email = email;
     this.nombrecompleto = function () {
-        return this.nombre + ' '+ this.apellido
+        return this.nombre + ' ' + this.apellido
     }
 }
-let padre = new Persona ("Camilo", "Rocha", "camiloerec@gmail.com");
-let personax = new Persona ()
+let padre = new Persona("Camilo", "Rocha", "camiloerec@gmail.com");
+let personax = new Persona()
 console.log(personax)
 console.log(padre);
 let madre = new Persona("Laura", "Contreras", "firmuoco@nili.kn");
 Persona.prototype.tel = 4432211;
 console.log(madre.tel)
 padre.tel = 12345
+
 console.log(madre)
 console.log(padre)
+
+
+
+
+function Fsaludar(nombre) {
+    nombre
+    return "Hola " + nombre;
+}
+console.log(Fsaludar("Eduard"))
+
+
+const cliente1 = {
+    saludar: Fsaludar
+}
+console.log(cliente1.saludar("Santi"))
+
+var constructor = function (precio) {
+    let miObjeto={
+        precioBase : precio,
+        iva: 1.21,
+        calcularIva: function () {
+            return (this.precioBase * this.iva)
+        }
+    }
+    return miObjeto;
+}
+precio1 =constructor(100);
+console.log(precio1.calcularIva());
