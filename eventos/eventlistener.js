d3e0bff18655942ccd6ff35cc8ee98600476959b
Eduard Murillo <murilloeduard3@gmail.com>
2024-10-20 15:21:25 -0500

    Add new file "eventListener" this a new example for js evenlistener

--- eventos/eventlistener.js
+++ eventos/eventlistener.js
@@ -1,8 +1,28 @@
 let boton = document.querySelector("#btnPrincipal")
+let boton2 = document.querySelector("#btnSecundario")
+let $botton = document.querySelector("#primero")
+let $segundo2 = document.querySelector ("#segundo")
+
 // boton.addEventListener("click", respuestaClick)
 // function respuestaClick() {
 //     alert("Respuesta al evento")
 // }
 boton.onclick = ()=>{
     alert("Respuesta al click ")
+}
+boton2.onclick = ()=>{
+    alert("Respuesta al click 3")
+}
+$botton.onclick = function() {
+    $botton.innerHTML = 'Ooops!';
+}
+
+$segundo2.onclick = function () {
+    let p = document.createElement("p");
+    p.innerHTML = `${displayDate()}`;
+    document.body.appendChild(p);
+}
+
+function displayDate() {
+    return new Date().toLocaleTimeString();
 }
